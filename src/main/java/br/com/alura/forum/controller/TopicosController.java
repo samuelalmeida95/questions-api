6c411eacdc840abf6441999786434f51c1e52635
samuelalmeida95 <samuka.ab.melo@hotmail.com>
2022-04-23 19:31:55 -0300

    adiciona dto para topicos e ajusta anotations do controller

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,24 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.controller.dto.TopicoDTO;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(value = "/topicos")
 public class TopicosController {
 
     @GetMapping("/listarTopicos")
-    @ResponseBody
-    public List<Topico> listar() {
+    public List<TopicoDTO> listar() {
         Topico t1 = new Topico("Duvida", "Duvida Spring", new Curso("Spring", "Programação"));
-        return Arrays.asList(t1, t1, t1, t1);
+        return TopicoDTO.converter(Arrays.asList(t1, t1, t1, t1));
     }
+
+
 }
---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDTO.java
@@ -0,0 +1,28 @@
+package br.com.alura.forum.controller.dto;
+
+import br.com.alura.forum.modelo.Topico;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class TopicoDTO {
+
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+
+    public TopicoDTO(Topico topico) {
+        this.id = topico.getId();
+        this.titulo = topico.getTitulo();
+        this.mensagem = topico.getMensagem();
+        this.dataCriacao = topico.getDataCriacao();
+    }
+
+    public static List<TopicoDTO> converter(List<Topico> topicos) {
+        return topicos.stream().map(TopicoDTO::new).collect(Collectors.toList());
+    }
+}
